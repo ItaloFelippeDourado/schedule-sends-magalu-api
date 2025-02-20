a9e88aa2d42c5c7f68ed820be6e97df70a69200b
corpp03605 <italodourado@ext.cvccorp.com.br>
2025-02-20 10:16:51 -0300

    first commit

---
+++ src/main/java/br/com/metaldome/schedule_sends_magalu_api/ScheduleSendsMagaluApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.metaldome.schedule_sends_magalu_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleSendsMagaluApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleSendsMagaluApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/metaldome/schedule_sends_magalu_api/ScheduleSendsMagaluApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.metaldome.schedule_sends_magalu_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleSendsMagaluApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
